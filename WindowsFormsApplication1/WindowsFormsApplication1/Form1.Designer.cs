ab3a04504ed7a215aae9a0259fde03206b189bd0
bilter1001 <bilter1001@163.com>
2014-09-30 16:42:14 +0800

    提交C#数据库操作代码

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            m_dbConnect.Close();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);           
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxZY1 = new System.Windows.Forms.TextBox();
+            this.textBoxZY2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxYF = new System.Windows.Forms.TextBox();
+            this.textBoxZZ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.richTextBoxZC = new RichTextBoxLinks.RichTextBoxEx();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "相加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(214, 11);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(76, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "=";
+            // 
+            // listBox1
+            // 
+            this.listBox1.ColumnWidth = 85;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "Item 1, column 1",
+            "Item 2, column 1",
+            "Item 3, column 1",
+            "Item 4, column 1",
+            "Item 5, column 1",
+            "Item 2, column 2",
+            "Item 3, column 2",
+            "Item 4, column 2"});
+            this.listBox1.Location = new System.Drawing.Point(12, 157);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(118, 268);
+            this.listBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(136, 157);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(406, 484);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            this.dataGridView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyUp);
+            // 
+            // textBoxZY1
+            // 
+            this.textBoxZY1.Location = new System.Drawing.Point(80, 74);
+            this.textBoxZY1.Name = "textBoxZY1";
+            this.textBoxZY1.Size = new System.Drawing.Size(89, 21);
+            this.textBoxZY1.TabIndex = 8;
+            // 
+            // textBoxZY2
+            // 
+            this.textBoxZY2.Location = new System.Drawing.Point(280, 74);
+            this.textBoxZY2.Name = "textBoxZY2";
+            this.textBoxZY2.Size = new System.Drawing.Size(92, 21);
+            this.textBoxZY2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 26);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "查询";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "中药1：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(212, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "中药2：";
+            // 
+            // textBoxYF
+            // 
+            this.textBoxYF.Location = new System.Drawing.Point(555, 357);
+            this.textBoxYF.Multiline = true;
+            this.textBoxYF.Name = "textBoxYF";
+            this.textBoxYF.ReadOnly = true;
+            this.textBoxYF.Size = new System.Drawing.Size(395, 126);
+            this.textBoxYF.TabIndex = 12;
+            // 
+            // textBoxZZ
+            // 
+            this.textBoxZZ.Location = new System.Drawing.Point(555, 532);
+            this.textBoxZZ.Multiline = true;
+            this.textBoxZZ.Name = "textBoxZZ";
+            this.textBoxZZ.ReadOnly = true;
+            this.textBoxZZ.Size = new System.Drawing.Size(395, 109);
+            this.textBoxZZ.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "方剂列表：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(553, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "组成：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(553, 332);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "用法：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(553, 507);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "主治：";
+            // 
+            // richTextBoxZC
+            // 
+            this.richTextBoxZC.Location = new System.Drawing.Point(555, 157);
+            this.richTextBoxZC.Name = "richTextBoxZC";
+            this.richTextBoxZC.ReadOnly = true;
+            this.richTextBoxZC.Size = new System.Drawing.Size(395, 155);
+            this.richTextBoxZC.TabIndex = 11;
+            this.richTextBoxZC.Text = "";
+            this.richTextBoxZC.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBoxZC_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 653);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxZZ);
+            this.Controls.Add(this.textBoxYF);
+            this.Controls.Add(this.richTextBoxZC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxZY2);
+            this.Controls.Add(this.textBoxZY1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+
+        private OleDbConnection m_dbConnect;
+        private OleDbCommand m_dbCommand;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxZY1;
+        private System.Windows.Forms.TextBox textBoxZY2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private RichTextBoxLinks.RichTextBoxEx richTextBoxZC;
+        private System.Windows.Forms.TextBox textBoxYF;
+        private System.Windows.Forms.TextBox textBoxZZ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using RichTextBoxLinks;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+
+    public class myComparerClass : IComparer<string>  
+    {
+
+          // Calls CaseInsensitiveComparer.Compare with the parameters reversed.
+//        int IComparer<string>.Compare(string x, string y)
+        public int Compare(string x, string y)
+        {
+            if (x.Length > y.Length)
+            {
+                return -1;
+            }
+            if (x.Length == y.Length)
+            {
+                return 0;
+            }
+            else
+                return 1;
+         }
+    }
+
+
+    public class myComparableClass : IComparable<myComparableClass>
+    {
+        string s;
+        public myComparableClass(string other)
+        {
+            s = other;
+        }
+
+
+        public int CompareTo(myComparableClass other)
+        {
+            if (s.Length > other.s.Length)
+            {
+                return -1;
+            }
+            if (s.Length == other.s.Length)
+            {
+                return 0;
+            }
+            else
+                return 1;
+        }
+    }
+
+
+
+
+    public partial class Form1 : Form
+    {
+
+ //       private System.Windows.Forms.LinkLabel linkLabel1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            string strConnection = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=FJ.accdb";
+            m_dbConnect = new OleDbConnection(strConnection);           
+            m_dbConnect.Open();
+            m_dbCommand = new OleDbCommand();
+            m_dbCommand.Connection = m_dbConnect;// m_dbCommand = m_dbConnect.CreateCommand();      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox1.Text + textBox2.Text;
+
+            long nNum1 = Convert.ToInt64(textBox1.Text);
+            long nNum2 = Convert.ToInt64(textBox2.Text);
+
+            long nNum3 = nNum1 + nNum2;
+            textBox3.Text = Convert.ToString(nNum3);
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar == '\b')//退格键
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        //查询
+        private void button2_Click(object sender, EventArgs e)
+        {      
+
+            string strSql = "select ID, Name, Source from 方剂大辞典 where Constituents like ";
+	        bool bIndex = false;
+
+	        if (textBoxZY1.Text.Length!=0)
+	        {
+                string strTemp = "'%" + textBoxZY1.Text + "%'";		
+		        strSql += strTemp;
+		        bIndex = true;
+	        }
+            if (textBoxZY2.Text.Length != 0)
+	        {
+                string strTemp = "";
+		        if (bIndex)
+		        {
+                    strTemp = " and Constituents like '%" + textBoxZY2.Text + "%'";
+		        }
+		        else
+		        {
+                    strTemp = "'%" + textBoxZY2.Text + "%'";
+		        }		
+		        strSql += strTemp;
+		        bIndex = true;
+	        }
+
+            m_dbCommand.CommandText = strSql;
+            try
+            {
+                //Access数据库适配器              
+                OleDbDataAdapter GetInformationFromAccess = new OleDbDataAdapter(m_dbCommand);
+                //创建一个Access表空间              
+                DataSet ds = new DataSet();
+                ds.Clear();
+                GetInformationFromAccess.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                //也可以使用下面方式设置数据源
+                //             GetInformationFromAccess.Fill(ds, "方剂大辞典");
+                //             dataGridView1.DataSource = ds;
+                //             dataGridView1.DataMember = "方剂大辞典";           
+
+                dataGridView1.Columns[0].Width = dataGridView1.Width / 6;//设置列宽
+                dataGridView1.Columns[1].HeaderText = "方剂名";//设置列名
+                dataGridView1.Columns[1].Width = dataGridView1.Width / 5 * 2;//设置列宽
+                dataGridView1.Columns[2].HeaderText = "方源";
+                dataGridView1.Columns[2].Width = dataGridView1.Width / 5 * 2;
+            }
+            catch (OleDbException ecp)
+            {
+                string errorMessages = "";
+
+                for (int i = 0; i < ecp.Errors.Count; i++)
+                {
+                    errorMessages += "Index #" + i + "\n" +
+                                     "Message: " + ecp.Errors[i].Message + "\n" +
+                                     "NativeError: " + ecp.Errors[i].NativeError + "\n" +
+                                     "Source: " + ecp.Errors[i].Source + "\n" +
+                                     "SQLState: " + ecp.Errors[i].SQLState + "\n";
+                }
+                //右击我的电脑->管理->事件查看器->日志->应用程序就可以了.
+//                 System.Diagnostics.EventLog log = new System.Diagnostics.EventLog();
+//                 log.Source = "My Application";
+//                 log.WriteEntry(errorMessages);
+                Console.WriteLine("An exception occurred. Please contact your system administrator.");
+            }
+           
+//             FontStyle fontstyle = new FontStyle();
+//             Font oldFont = this.richTextBoxZC.SelectionFont;
+//             Font newFont;
+//             if (oldFont.Underline)
+//                 newFont = new Font(oldFont, oldFont.Style & ~FontStyle.Underline);
+//             else
+//                 newFont = new Font(oldFont, oldFont.Style | FontStyle.Underline );
+//             richTextBoxZC.Select(0, 3);
+//             richTextBoxZC.SelectionFont = newFont;
+//              richTextBoxZC.Focus();  
+//             LinkLabel linkLabel1 = new LinkLabel();
+//             linkLabel1.Text = "www.baidu.com";
+//             linkLabel1.LinkClicked += new LinkLabelLinkClickedEventHandler(linkLabel1_LinkClicked);
+//             this.richTextBoxZC.Controls.Add(linkLabel1);
+
+
+//             richTextBoxZC.Text = "haih  haha";
+//             richTextBoxZC.Select(0, 3);
+//             richTextBoxZC.SetSelectionLink(true);
+//             richTextBoxZC.Select(0, 0);
+
+            dataGridView1.Focus();
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited.
+            string strText = ((LinkLabel)sender).Text;
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start(((LinkLabel)sender).Text);
+        }
+
+        private void richTextBoxZC_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            string strLink =  e.LinkText;
+        }
+
+        //根据规则拆分字符串
+        private void SplitString()
+        {
+//             string strTest = "dddad,a,,gadf";
+//             string[] strArrayTest = strTest.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            string[] strArrayTest = { "abc", "ab", "abcd", "a", "abcde", "abd" };
+            Array.Sort(strArrayTest);
+            IComparer<string> myComparer = new myComparerClass();
+            Array.Sort(strArrayTest,myComparer);
+
+            myComparableClass[] myComparableClassArr = new myComparableClass[6];
+            myComparableClassArr[0] = new myComparableClass("abc");
+            myComparableClassArr[1] = new myComparableClass("ab");
+            myComparableClassArr[2] = new myComparableClass("abcd");
+            myComparableClassArr[3] = new myComparableClass("a");
+            myComparableClassArr[4] = new myComparableClass("abcde");
+            myComparableClassArr[5] = new myComparableClass("abc");
+            Array.Sort(myComparableClassArr);
+
+
+
+
+            string[] strArray = richTextBoxZC.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);//StringSplitOptions.RemoveEmptyEntries去除空的数组
+            foreach (string strI in strArray)
+            {
+                int nStart = richTextBoxZC.Find(strI);
+                if (nStart != -1)
+                {
+                    richTextBoxZC.Select(nStart, strI.Length);
+                    richTextBoxZC.SetSelectionLink(true);
+                }
+
+            }
+            richTextBoxZC.Select(0, 0);         
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            string strID = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string strsql = "select * from 方剂大辞典 where ID =" + strID;
+            m_dbCommand.CommandText = strsql;
+            OleDbDataReader oleReader = m_dbCommand.ExecuteReader();
+
+            while (oleReader.Read())
+            {
+                if (!oleReader.IsDBNull(3))//判断该字段的值是否为空
+                {
+                    string strConstituents = oleReader.GetString(3);
+                    richTextBoxZC.Text = strConstituents;
+                }
+                else
+                {
+                    richTextBoxZC.Clear();
+                }
+                if (!oleReader.IsDBNull(4))
+                {
+                    string strUsage = oleReader.GetString(4);
+                    textBoxYF.Text = strUsage;
+                }
+                else
+                {
+                    textBoxYF.Clear();
+                }
+                if (!oleReader.IsDBNull(5))
+                {
+                    string strIndications = oleReader.GetString(5);
+                    textBoxZZ.Text = strIndications;
+                }
+                else
+                {
+                    textBoxZZ.Clear();
+                }
+            }
+            oleReader.Close();
+            SplitString();
+        }
+
+        private void dataGridView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            string strID = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+               
+            string strsql = "select * from 方剂大辞典 where ID =" + strID;
+            m_dbCommand.CommandText = strsql;
+            OleDbDataReader oleReader = m_dbCommand.ExecuteReader();
+
+            while (oleReader.Read())
+            {
+                if (!oleReader.IsDBNull(3))//判断该字段的值是否为空
+                {
+                    string strConstituents = oleReader.GetString(3);
+                    richTextBoxZC.Text = strConstituents;
+                }
+                else
+                {
+                    richTextBoxZC.Clear();
+                }
+                if (!oleReader.IsDBNull(4))
+                {
+                    string strUsage = oleReader.GetString(4);
+                    textBoxYF.Text = strUsage;
+                }
+                else
+                {
+                    textBoxYF.Clear();
+                }
+                if (!oleReader.IsDBNull(5))
+                {
+                    string strIndications = oleReader.GetString(5);
+                    textBoxZZ.Text = strIndications;
+                }
+                else
+                {
+                    textBoxZZ.Clear();
+                }
+            }
+            oleReader.Close();
+            SplitString();
+
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string strID = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+            string strsql = "select * from 方剂大辞典 where ID =" + strID;
+            m_dbCommand.CommandText = strsql;
+            OleDbDataReader oleReader = m_dbCommand.ExecuteReader();
+
+            while (oleReader.Read())
+            {
+                if (!oleReader.IsDBNull(3))//判断该字段的值是否为空
+                {
+                    string strConstituents = oleReader.GetString(3);
+                    richTextBoxZC.Text = strConstituents;
+                }
+                else
+                {
+                    richTextBoxZC.Clear();
+                }
+                if (!oleReader.IsDBNull(4))
+                {
+                    string strUsage = oleReader.GetString(4);
+                    textBoxYF.Text = strUsage;
+                }
+                else
+                {
+                    textBoxYF.Clear();
+                }
+                if (!oleReader.IsDBNull(5))
+                {
+                    string strIndications = oleReader.GetString(5);
+                    textBoxZZ.Text = strIndications;
+                }
+                else
+                {
+                    textBoxZZ.Clear();
+                }
+            }
+            oleReader.Close();
+            SplitString();
+
+        }
+
+    }
+}
